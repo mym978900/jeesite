9a9c164d205278a0018a9109dac0ad5e5c8b42a2
Administrator <Administrator@HFJS-2019DWYVSD>
2019-11-29 17:53:10 +0800

    test

--- web/src/main/java/com/jeesite/modules/Application.java
+++ web/src/main/java/com/jeesite/modules/Application.java
@@ -14,6 +14,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @version 2018-10-13
  */
 @SpringBootApplication
+@ServletComponentScan
 public class Application extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
